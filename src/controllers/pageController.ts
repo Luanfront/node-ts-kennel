f46825bdc3264ae9c545657658840e278b5f9013
Luan Silva <luansantosdassilva@gmail.com>
2025-01-31 16:00:46 -0300

    modified:   _html/index.html 	modified:   src/controllers/pageController.ts 	modified:   src/routes/index.ts 	modified:   src/server.ts

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,15 +1,15 @@
 import { Request , Response } from 'express';
 
-export const home = (req:Request, res:Response) => {
+export const home = (req: Request, res: Response) => {
     res.render('pages/page');
 }
-export const dogs = (req:Request, res: Response) => {
+export const dogs = (req: Request, res: Response) => {
     res.send('dogs');
 }
-export const cats = (req:Request, res: Response) => {
+export const cats = (req: Request, res: Response) => {
     res.send('cats');
    
 }
-export const fishes = (req:Request, res: Response) => {
+export const fishes = (req: Request, res: Response) => {
    res.send('fishes');
 }
